0d2845dd76b25fad63ad995ecab0eca9354b3e09
Miraflor G. Buizon <mira.cute25@yahoo.com>
2014-08-14 11:27:36 +0800

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miraf::Application.config.secret_key_base = 'bb52e342b17a53cf28f0b727ea1fb65335c43111c5c63c3c72a8ccbbb39504845c62440c754f5e87e8d000aa60480ccdeeb99dceb9b8ed9698cadceef053f7fa'
